c581f9764b2e5dcc06027a67ddca3aadf0b4a039
tscott-au <tim@nepelle.com.au>
2018-11-08 15:24:25 +1100

    new nerves project

---
+++ lib/nerves_signal.ex
@@ -0,0 +1,18 @@
+defmodule NervesSignal do
+  @moduledoc """
+  Documentation for NervesSignal.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesSignal.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_signal_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesSignalTest do
+  use ExUnit.Case
+  doctest NervesSignal
+
+  test "greets the world" do
+    assert NervesSignal.hello() == :world
+  end
+end
